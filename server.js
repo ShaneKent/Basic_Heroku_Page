3e6423a52ce91f88f10be5e1a3afcf495c949a41
ShaneKent <sdkent@calpoly.edu>
2016-04-18 21:31:51 -0700

    Eddited server.js to run correctly?

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ function send404Response(response){
 
 function onRequest(request, response){
 	console.log("A user made a request... " + request.url);
-	
+
 	if (request.method == "GET" && request.url == "/"){
 		response.writeHead(200, {"Context-Type": "text/html"});
 		fs.createReadStream("./index.html").pipe(response);
@@ -21,5 +21,30 @@ function onRequest(request, response){
 	}
 }
 
-http.createServer(onRequest).listen(5000);
-console.log("Server is now running...");
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  //response.render('pages/index');
+  /*var result = '';
+  var times = process.env.TIMES || 5;
+  for (i = 0; i < times; i++){
+     result += (cool() + " ");
+  }
+  response.send(result);*/
+	response.writeHead(200, {"Context-Type": "text/plain"});
+	response.write("Test!");
+	response.end();
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
