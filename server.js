cfa7784986de01957e7a4c6c738b266bf16f5001
ShaneKent <sdkent@calpoly.edu>
2016-04-18 21:23:09 -0700

    Basic Node Server running...

---
+++ server.js
@@ -0,0 +1,25 @@
+var http = require("http");
+var fs = require("fs");
+
+function send404Response(response){
+	response.writeHead(404, {"Context-Type": "text/plain"});
+	response.write("Error 404: Page not found!");
+	response.end();
+}
+
+function onRequest(request, response){
+	console.log("A user made a request... " + request.url);
+	
+	if (request.method == "GET" && request.url == "/"){
+		response.writeHead(200, {"Context-Type": "text/html"});
+		fs.createReadStream("./index.html").pipe(response);
+	} else if (request.method = "GET" && request.url == "/home.html"){
+		response.writeHead(200, {"Content-Type": "text/html"});
+		fs.createReadStream("./home.html").pipe(response);
+	} else {
+		send404Response(response);
+	}
+}
+
+http.createServer(onRequest).listen(2222);
+console.log("Server is now running...");
