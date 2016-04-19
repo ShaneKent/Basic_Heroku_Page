a14dcd6b611dd1b6f0d7fc4608d3b49180cb4447
ShaneKent <sdkent@calpoly.edu>
2016-04-18 21:35:10 -0700

    Cut down server.js

--- server.js
+++ server.js
@@ -1,48 +1,19 @@
-var http = require("http");
 var fs = require("fs");
-
-function send404Response(response){
-	response.writeHead(404, {"Context-Type": "text/plain"});
-	response.write("Error 404: Page not found!");
-	response.end();
-}
-
-function onRequest(request, response){
-	console.log("A user made a request... " + request.url);
-
-	if (request.method == "GET" && request.url == "/"){
-		response.writeHead(200, {"Context-Type": "text/html"});
-		fs.createReadStream("./index.html").pipe(response);
-	} else if (request.method = "GET" && request.url == "/home.html"){
-		response.writeHead(200, {"Content-Type": "text/html"});
-		fs.createReadStream("./home.html").pipe(response);
-	} else {
-		send404Response(response);
-	}
-}
-
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-//app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-//app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
-
 app.get('/', function(request, response) {
-  //response.render('pages/index');
-  /*var result = '';
-  var times = process.env.TIMES || 5;
-  for (i = 0; i < times; i++){
-     result += (cool() + " ");
-  }
-  response.send(result);*/
-	response.writeHead(200, {"Context-Type": "text/plain"});
-	response.write("Test!");
-	response.end();
+	console.log("A user made a request... " + request.url);
+	response.writeHead(200, {"Context-Type": "text/html"});
+	fs.createReadStream("./index.html").pipe(response);
+});
+
+app.get('/home.html', function(request, response) {
+	console.log("A user made a request... " + request.url);
+	response.writeHead(200, {"Content-Type": "text/html"});
+	fs.createReadStream("./home.html").pipe(response);
 });
 
 app.listen(app.get('port'), function() {
