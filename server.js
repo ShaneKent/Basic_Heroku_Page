b4cbef5e8a1a0671a0e119b1dc147ccf7cb7d51a
ShaneKent <sdkent@calpoly.edu>
2016-04-20 14:49:50 -0700

    Changed how server is dealing with connecting to pages. Changed anonymous functions for real functions:    returnIndexHtml();    returnHomeHtml();

--- server.js
+++ server.js
@@ -2,20 +2,24 @@ var fs = require("fs");
 var express = require('express');
 var app = express();
 
+//Set the server port to be on 5000. e.g. localhost:5000
 app.set('port', (process.env.PORT || 5000));
-
-app.get('/', function(request, response) {
-	console.log("A user made a request... " + request.url);
-	response.writeHead(200, {"Context-Type": "text/html"});
-	fs.createReadStream("./index.html").pipe(response);
-});
-
-app.get('/home.html', function(request, response) {
-	console.log("A user made a request... " + request.url);
-	response.writeHead(200, {"Content-Type": "text/html"});
-	fs.createReadStream("./home.html").pipe(response);
-});
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+function returnIndexHtml(request, response){
+	console.log("A user made a request... " + request.url);
+	response.writeHead(200, {"Content-Type": "text/html"});
+	fs.createReadStream("./index.html").pipe(response);
+}
+
+function returnHomeHtml(request, response){\
+	console.log("A user made a request... " + request.url);
+	response.writeHead(200, {"Content-Type": "text/html"});
+	fs.createReadStream("./home.html").pipe(response);
+}
+
+app.get('/', returnIndexHtml);
+app.get('/index.html', returnIndexHtml);
+app.get('/home.html', returnHomeHtml);
